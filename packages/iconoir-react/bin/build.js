15b97e9c52d11ef30381edecad90ecf17b77c335
Pascal Jufer <pascal-jufer@bluewin.ch>
2021-05-28 15:23:32 +0200

    Better error handling/cleanup, add spinner

--- packages/iconoir-react/bin/build.js
+++ packages/iconoir-react/bin/build.js
@@ -2,6 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const fsPromises = require('fs').promises;
 const { exec } = require('child_process');
+const ora = require('ora');
 
 const rootDir = path.join(__dirname, '..');
 const iconoirRootDir = path.join(rootDir, '../..');
@@ -21,17 +22,19 @@ const incompatibleNames = {
   youtube: 'youTube',
 };
 
-if (!fs.existsSync(tmpIconsDir)) {
-  fs.mkdirSync(tmpIconsDir);
-}
+const spinner = ora();
 
 fs.readdir(iconoirIconsDir, (err, files) => {
   if (err) {
-    return console.error('Unable to find root icons directory');
+    throw new Error('Unable to find root icons directory');
   }
 
-  const promises = [];
+  if (!fs.existsSync(tmpIconsDir)) {
+    fs.mkdirSync(tmpIconsDir);
+  }
 
+  spinner.start('Copying icons to tmp dir, while renaming files with incompatible names');
+  const promises = [];
   files.forEach((file) => {
     const srcFilePath = path.join(iconoirIconsDir, file);
     const iconName = file.split('.')[0];
@@ -42,25 +45,44 @@ fs.readdir(iconoirIconsDir, (err, files) => {
     promises.push(fsPromises.copyFile(srcFilePath, dstFilePath));
   });
 
-  Promise.all(promises).then(() => {
+  function rmTmpDir() {
+    fs.rm(tmpIconsDir, { recursive: true }, (err) => {
+      if (err) {
+        console.error('Could not remove tmp dir');
+      }
+    });
+  }
+
+  Promise.all(promises)
+    .then(() => {
+    spinner.succeed();
+
+    spinner.start('Generating icons');
     exec(
-      `npx @svgr/cli --out-dir ${builtIconsDir} ${tmpIconsDir}`,
+      `npx @svgr/cli --silent --out-dir ${builtIconsDir} ${tmpIconsDir}`,
       (err, stdout, stderr) => {
         if (err) {
-          return console.error('Could not run command to generate icons');
+          spinner.stop();
+          rmTmpDir();
+          throw new Error('Could not run command to generate icons');
         }
+
+        spinner.succeed();
+
         if (stdout) {
           console.log(stdout);
         }
         if (stderr) {
           console.error(`Error while generating icons:\n${stderr}`);
         }
-        fs.rm(tmpIconsDir, { recursive: true }, (err) => {
-          if (err) {
-            console.error('Could not remove tmp dir');
-          }
-        });
+
+        rmTmpDir();
       }
     );
+  })
+  .catch(() => {
+    spinner.stop();
+    rmTmpDir();
+    throw new Error('Error while copying icons');
   });
 });
