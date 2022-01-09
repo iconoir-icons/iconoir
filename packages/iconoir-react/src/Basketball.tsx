5a5415ed3b6a06a2683cae7ed3d8f468fddd083f
lucaburgio <lucaburgio@users.noreply.github.com>
2022-01-09 10:03:17 +0000

    Update build artifacts

--- packages/iconoir-react/src/Basketball.tsx
+++ packages/iconoir-react/src/Basketball.tsx
@@ -1,30 +1,30 @@
 import * as React from 'react';
-import Svg, { SvgProps, Path } from 'react-native-svg';
 
-function SvgBasketBall(
-  props: SvgProps,
-  svgRef?: React.Ref<React.Component<SvgProps>>
+function SvgBasketball(
+  props: React.SVGProps<SVGSVGElement>,
+  svgRef?: React.Ref<SVGSVGElement>
 ) {
   return (
-    <Svg
+    <svg
       width="1.5em"
       height="1.5em"
       strokeWidth={1.5}
       viewBox="0 0 24 24"
       fill="none"
+      xmlns="http://www.w3.org/2000/svg"
       color="currentColor"
       ref={svgRef}
       {...props}
     >
-      <Path
+      <path
         d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zM12 22V2M21.95 11c-6.47 2.667-12.254 2.667-19.9 0M18.572 4.462c-2.667 4.53-2.667 9.723 0 15.076M5.428 4.462c2.667 4.53 2.667 9.723 0 15.076"
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-    </Svg>
+    </svg>
   );
 }
 
-const ForwardRef = React.forwardRef(SvgBasketBall);
+const ForwardRef = React.forwardRef(SvgBasketball);
 export default ForwardRef;
--- packages/iconoir-react/src/BasketballAlt.tsx
+++ packages/iconoir-react/src/BasketballAlt.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-function SvgBasketBallAlt(
+function SvgBasketballAlt(
   props: React.SVGProps<SVGSVGElement>,
   svgRef?: React.Ref<SVGSVGElement>
 ) {
@@ -17,7 +17,7 @@ function SvgBasketBallAlt(
       {...props}
     >
       <g
-        clipPath="url(#basket-ball-alt_svg__clip0)"
+        clipPath="url(#basketball-alt_svg__clip0)"
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
@@ -25,7 +25,7 @@ function SvgBasketBallAlt(
         <path d="M17.736 20.192c4.524-3.168 5.623-9.404 2.455-13.928C17.024 1.74 10.788.641 6.264 3.81 1.74 6.976.641 13.212 3.808 17.736c3.168 4.524 9.404 5.623 13.928 2.456zM17.736 20.192L6.264 3.809M19.577 5.473c-3.77 5.896-8.508 9.214-16.302 11.415M13.06 2.056c.413 5.24 3.392 9.494 8.646 12.35M2.293 9.595c4.783 2.18 7.761 6.434 8.647 12.349" />
       </g>
       <defs>
-        <clipPath id="basket-ball-alt_svg__clip0">
+        <clipPath id="basketball-alt_svg__clip0">
           <path fill="#fff" d="M0 0h24v24H0z" />
         </clipPath>
       </defs>
@@ -33,5 +33,5 @@ function SvgBasketBallAlt(
   );
 }
 
-const ForwardRef = React.forwardRef(SvgBasketBallAlt);
+const ForwardRef = React.forwardRef(SvgBasketballAlt);
 export default ForwardRef;
