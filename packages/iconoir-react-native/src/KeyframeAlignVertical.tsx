0be5ba347d1f149649559d30d8878aa5a2232019
paescuj <paescuj@users.noreply.github.com>
2022-01-10 15:50:39 +0000

    Update build artifacts

--- packages/iconoir-react-native/src/KeyframeAlignVertical.tsx
+++ packages/iconoir-react-native/src/KeyframeAlignVertical.tsx
@@ -1,30 +1,30 @@
 import * as React from 'react';
+import Svg, { SvgProps, Path } from 'react-native-svg';
 
-function SvgKeyframeAlignHorizontal1(
-  props: React.SVGProps<SVGSVGElement>,
-  svgRef?: React.Ref<SVGSVGElement>
+function SvgKeyframeAlignVertical(
+  props: SvgProps,
+  svgRef?: React.Ref<React.Component<SvgProps>>
 ) {
   return (
-    <svg
+    <Svg
       width="1.5em"
       height="1.5em"
       strokeWidth={1.5}
       viewBox="0 0 24 24"
       fill="none"
-      xmlns="http://www.w3.org/2000/svg"
       color="currentColor"
       ref={svgRef}
       {...props}
     >
-      <path
+      <Path
         d="M15.7 12.375l-3.231 4.04a.6.6 0 01-.938 0L8.3 12.374a.6.6 0 010-.75l3.231-4.04a.6.6 0 01.938 0l3.231 4.04a.6.6 0 010 .75zM12 22v-2M12 4V2"
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-    </svg>
+    </Svg>
   );
 }
 
-const ForwardRef = React.forwardRef(SvgKeyframeAlignHorizontal1);
+const ForwardRef = React.forwardRef(SvgKeyframeAlignVertical);
 export default ForwardRef;
