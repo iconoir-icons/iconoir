a4da510074194553658d4c0c2f10934f06f16819
heavybeard <info@heavybeard.it>
2022-06-30 12:05:08 +0200

    fix(flutter): remove generated stuff and add flutter_svg dep

--- packages/iconoir-flutter/lib/iconoir_flutter.dart
+++ packages/iconoir-flutter/lib/iconoir_flutter.dart
@@ -1,7 +1,1 @@
 library iconoir_flutter;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
--- packages/iconoir-flutter/test/iconoir_flutter_test.dart
+++ packages/iconoir-flutter/test/iconoir_flutter_test.dart
@@ -1,12 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:iconoir_flutter/iconoir_flutter.dart';
-
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
+  test('mock', () {}, skip: true);
 }
