f2670e2583bb17508b2f689bc4a2a3d92c35bab0
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-01-10 11:42:59 +0100

    fix: wrap app in SSRProvider
    Addresses the following warning:
    > When server rendering, you must wrap your application in an <SSRProvider> to ensure consistent ids are generated between the client and server.

--- iconoir.com/pages/_app.tsx
+++ iconoir.com/pages/_app.tsx
@@ -1,9 +1,14 @@
 import type { AppProps } from 'next/app';
 import '../styles/prism-theme.css';
 import '../styles/theme.css';
+import { SSRProvider } from '@react-aria/ssr';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SSRProvider>
+      <Component {...pageProps} />
+    </SSRProvider>
+  );
 }
 
 export default MyApp;
