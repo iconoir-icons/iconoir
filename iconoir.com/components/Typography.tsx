78376407b54794e166e18416f8e48a66238d803d
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-09-25 05:00:02 +0200

    fix: lint issue in website

--- iconoir.com/components/Typography.tsx
+++ iconoir.com/components/Typography.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { showNotification } from '../lib/showNotification';
 import { CopyButton } from './Button';
 import { media } from '../lib/responsive';
