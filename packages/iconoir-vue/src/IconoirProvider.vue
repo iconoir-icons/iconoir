41facbefb99f819d326794f33e4a7bc87a9eb15b
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-10-18 12:10:12 +0200

    Fix gitignore

---
+++ packages/iconoir-vue/src/IconoirProvider.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { provide } from 'vue-demi';
+import type { SVGAttributes } from 'vue-demi';
+import providerKey from './providerKey';
+interface Props {
+  iconProps: SVGAttributes;
+}
+const props = defineProps<Props>();
+provide(providerKey, props.iconProps);
+</script>
+<template>
+  <slot />
+</template>
