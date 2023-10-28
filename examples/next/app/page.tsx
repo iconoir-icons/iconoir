e0437a7ceffc2d3301ba4fa448edea5ca3ab8d26
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-10-29 01:06:48 +0200

    Add provider to examples

--- examples/next/app/page.tsx
+++ examples/next/app/page.tsx
@@ -1,4 +1,10 @@
-import { Iconoir, Medal1st, Medal1stSolid } from 'iconoir-react';
+import {
+  Check,
+  Iconoir,
+  IconoirProvider,
+  Medal1st,
+  Medal1stSolid,
+} from 'iconoir-react';
 import { AdobeAfterEffects as AdobeAfterEffectsRegular } from 'iconoir-react/regular';
 import { AdobeAfterEffects as AdobeAfterEffectsSolid } from 'iconoir-react/solid';
 
@@ -6,10 +12,21 @@ export default function Home() {
   return (
     <>
       <Iconoir />
-      <Medal1st />
+      <Medal1st color="red" height={36} width={36} />
       <Medal1stSolid />
       <AdobeAfterEffectsRegular color="red" />
       <AdobeAfterEffectsSolid color="green" />
+
+      <IconoirProvider
+        iconProps={{
+          color: '#1E441E',
+          strokeWidth: 1,
+          width: '2em',
+          height: '2em',
+        }}
+      >
+        <Check />
+      </IconoirProvider>
     </>
   );
 }
