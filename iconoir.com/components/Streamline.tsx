0f5f811e175d8e28eeba0e1965544a02e2778c0b
Luca Burgio <burgio.luca@gmail.com>
2024-02-10 12:20:50 +0100

    fix: streamline sponsor

--- iconoir.com/components/Streamline.tsx
+++ iconoir.com/components/Streamline.tsx
@@ -3,11 +3,7 @@ import styled from 'styled-components';
 
 export function Streamline() {
   return (
-    <a
-      rel="sponsored"
-      href="https://bit.ly/3SNgpKo"
-      style={{ textDecoration: 'none' }}
-    >
+    <a href="https://bit.ly/3SNgpKo" style={{ textDecoration: 'none' }}>
       <PromoContainer>
         <PromoContent>
           <PromoInfo>
