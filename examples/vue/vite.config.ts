51a93353db492262dd6aa0694eb4719324f22d2c
Pascal Jufer <pascal-jufer@bluewin.ch>
2024-12-17 02:40:33 +0100

    Update example next

--- examples/vue/vite.config.ts
+++ examples/vue/vite.config.ts
@@ -1,11 +1,14 @@
 import { fileURLToPath, URL } from 'node:url';
+
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vueDevTools(),
   ],
   resolve: {
     alias: {
